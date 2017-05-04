17623198bdd25728a57f346bbdf5c746a6340f78
Ben Gamari <ben@smart-cactus.org>
2017-05-04 12:19:17 -0400

    Add script to verify links

---
+++ check.sh
@@ -0,0 +1,6 @@
+#!/bin/bash -e
+
+# docs/ seems to be a URL rewrite rule provided by haskell.org
+# dist/mac_frameworks appears to be lost to history
+linkchecker http://localhost:8000 --ignore-url 'docs/' --ignore-url=dist/mac_frameworks
+
