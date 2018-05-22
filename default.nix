15d33e23ee35c72327a3a2e8f4b040bc56285305
Ben Gamari <ben@smart-cactus.org>
2018-05-22 15:37:22 +0000

    nix: Provide upload script

--- default.nix
+++ default.nix
@@ -3,15 +3,16 @@
 with nixpkgs;
 let
   hs = haskellPackages.callCabal2nix "ghc-homepage" ./. {};
-in stdenv.mkDerivation {
-  name = "ghc-homepage-utils";
-  buildInputs = [ hs ];
-  nativeBuildInputs = [ makeWrapper ];
-  src = ./.;
-  installPhase = ''
-    mkdir $out
-    substitute upload.sh $out/upload.sh --replace sshfs ${sshfs}/bin/sshfs
-    chmod ugo+rx $out/upload.sh
-  '';
-}
+  upload = stdenv.mkDerivation {
+    name = "ghc-homepage-upload";
+    buildInputs = [ sshfs ];
+    nativeBuildInputs = [ makeWrapper ];
+    src = ./.;
+    installPhase = ''
+      mkdir -p $out/bin
+      substitute upload.sh $out/bin/upload.sh --replace sshfs ${sshfs}/bin/sshfs
+      chmod ugo+rx $out/bin/upload.sh
+    '';
+  };
 
+in buildEnv { name = "ghc-homepage-utils"; paths = [ hs upload ]; }
